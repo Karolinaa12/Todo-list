796c100632dde109863a7306623718f5fa077449
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-04-03 22:54:54 +0200

    listing all todos

--- todos.js
+++ todos.js
@@ -1,5 +1,13 @@
 let input = prompt("What would you like to do?");
-while (input !== "quit") {
+const todos = ["Collect eggs", "Clean room"];
+while (input !== "quit" && input !== "q") {
+  if (input == "list") {
+    console.log("********");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("********");
+  }
   input = prompt("What would you like to do?");
 }
 console.log("You quit the app");
