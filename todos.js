86e7f34a6708f20d5daecadf3a09fe8784291e9f
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-04-14 15:44:38 +0200

    Added new and delete functionality

--- todos.js
+++ todos.js
@@ -1,13 +1,46 @@
+///let input = prompt("What would you like to do?");
+//const todos = ["Collect eggs", "Clean room"];
+//while (input !== "quit" && input !== "q") {
+//if (input == "list") {
+//console.log("********");
+//for (let i = 0; i < todos.length; i++) {
+//  console.log(`${i}: ${todos[i]}`);
+// }
+//console.log("********");
+//}
+//input = prompt("What would you like to do?");
+//}
+//console.log("You quit the app");
+
 let input = prompt("What would you like to do?");
-const todos = ["Collect eggs", "Clean room"];
+const todos = ["Clean apartment", "Go shopping", "Bake cookies"];
+
 while (input !== "quit" && input !== "q") {
   if (input == "list") {
-    console.log("********");
+    console.log("**********");
     for (let i = 0; i < todos.length; i++) {
       console.log(`${i}: ${todos[i]}`);
     }
-    console.log("********");
+    console.log("**********");
+  } else if (input == "new") {
+    const newTodo = prompt("Add a to do");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list`);
+    console.log(todos);
+  } else if (input == "delete") {
+    const deleteIndex = parseInt(prompt("Add index to delete"));
+    if (!deleteIndex) {
+      console.log("Unknown index");
+    } else {
+      todos.splice(deleteIndex, 1);
+      console.log(`Todo number ${deleteIndex} deleted`);
+      console.log(todos);
+    }
+  } else {
+    input = prompt("I can't understand. Try again or type q to quit");
   }
+
   input = prompt("What would you like to do?");
 }
+
 console.log("You quit the app");
